c27ec76bf4d13fc4580926821f69521eb387e23a
Savya Patel <147362452+savya-patel@users.noreply.github.com>
2024-11-20 17:01:45 -0600

    chnages to L4 final (nothing)

--- basics/L4_final_project_lidar.py
+++ basics/L4_final_project_lidar.py
@@ -3,5 +3,11 @@ import L1_lidar
 import L3_path_template
 #returns return 'vec' which passes the closest valid vector [m, deg]
 import L2_vector
+import lidar_driving
 
 manual.main()
+#get an array of distances 
+#go to the closest obstance and maintain a distance of 0.4m 
+#logic to find straight lines 
+#untill 0.4 to oj both side sensors HC-SR04
+#third sensor in front to go in line
